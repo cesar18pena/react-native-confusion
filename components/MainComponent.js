aaacfb43e1792b9a0d58f8296b7d2e2b31d565d8
cesar18pena <cesar18pena@gmail.com>
2019-08-11 13:19:14 -0400

    Feat: use DrawerNavigator to create a Main menu for the app

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { View, Platform } from 'react-native';
 import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+import { Icon } from 'react-native-elements';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
 
@@ -20,12 +22,46 @@ const MenuNavigator = createStackNavigator({
   }
 });
 
+const HomeNavigator = createStackNavigator({
+  Home: { screen: Home }
+}, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+        backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+        color: "#fff"            
+    },
+    headerTintColor: "#fff"  
+  })
+});
+
+const MainNavigator = createDrawerNavigator({
+  Home: 
+    { screen: HomeNavigator,
+      navigationOptions: {
+        title: 'Home',
+        drawerLabel: 'Home'
+      }
+    },
+  Menu: 
+    { screen: MenuNavigator,
+      navigationOptions: {
+        title: 'Menu',
+        drawerLabel: 'Menu'
+      }, 
+    }
+}, {
+drawerBackgroundColor: '#D1C4E9'
+});
+
+
 class Main extends Component {
  
   render() {
     return (
       <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>        
-        <MenuNavigator />
+        <MainNavigator />
       </View>
     )
   }
