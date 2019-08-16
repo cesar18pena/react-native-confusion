7ff1370765e2b3e201618e14c90461a06a13cc1b
cesar18pena <cesar18pena@gmail.com>
2019-08-16 14:06:18 -0400

    Feat: start adding animations to Home Component

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, ScrollView, View } from 'react-native';
+import { Text, ScrollView, View, Animated, Easing } from 'react-native';
 import { Card } from 'react-native-elements';
 import { connect } from 'react-redux';
 import Loading from './LoadingComponent';
@@ -14,24 +14,24 @@ const mapStateToProps = state => {
 }
 
 const RenderItem = (props) => {
-    const item = props.item;
+  const item = props.item;
 
-    if (props.isLoading) {
-      return(
-        <Loading />
-      );
-    }
-    else if (props.errMess) {
-      return(
-        <View> 
-          <Text>{props.errMess}</Text>
-        </View>
-      );
-    }
-    
-    else {
-      if (item != null) {
-      return(
+  if (props.isLoading) {
+    return (
+      <Loading />
+    );
+  }
+  else if (props.errMess) {
+    return (
+      <View>
+        <Text>{props.errMess}</Text>
+      </View>
+    );
+  }
+
+  else {
+    if (item != null) {
+      return (
         <Card
           featuredTitle={item.name}
           featuredSubtitle={item.designation}
@@ -40,46 +40,83 @@ const RenderItem = (props) => {
           }}
         >
           <Text
-            style={{margin: 10}}>
+            style={{ margin: 10 }}>
             {item.description}
           </Text>
         </Card>
       );
     }
     else {
-      return(<View></View>);
+      return (<View></View>);
     }
   }
 }
 
 class Home extends Component {
 
-    static navigationOptions = {
-      title: 'Home',
-    };
+  constructor(props) {
+    super(props);
+    this.animatedValue = new Animated.Value(0);
+  }
 
-    render() {
 
-      return(
-        <ScrollView>
-          <RenderItem 
-            item={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
-            isLoading={this.props.dishes.isLoading}
-            errMess={this.props.dishes.errMess} 
-          />
-          <RenderItem 
-            item={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
-            isLoading={this.props.promotions.isLoading}
-            errMess={this.props.promotions.errMess} 
-          />
-          <RenderItem 
-            item={this.props.leaders.leaders.filter((leader) => leader.featured)[0]}
-            isLoading={this.props.leaders.isLoading}
-            errMess={this.props.leaders.errMess} 
-          />
-        </ScrollView>
-      );
-    }
+  componentDidMount() {
+    this.animate()
+  }
+
+  animate() {
+    this.animatedValue.setValue(0)
+    Animated.timing(
+      this.animatedValue,
+      {
+        toValue: 8,
+        duration: 8000,
+        easing: Easing.linear
+      }
+    ).start(() => this.animate())
+  }
+
+  static navigationOptions = {
+    title: 'Home',
+  };
+
+  render() {
+
+    const xpos1 = this.animatedValue.interpolate({
+      inputRange: [0, 1, 3, 5, 8],
+      outputRange: [1200, 600, 0, -600, -1200]
+    });
+
+    const xpos2 = this.animatedValue.interpolate({
+      inputRange: [0, 2, 4, 6, 8],
+      outputRange: [1200, 600, 0, -600, -1200]
+    });
+    
+    const xpos3 = this.animatedValue.interpolate({
+      inputRange: [0, 3, 5, 7, 8],
+      outputRange: [1200, 600, 0, -600, -1200]
+    });
+
+    return (
+      <ScrollView>
+        <RenderItem
+          item={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+          isLoading={this.props.dishes.isLoading}
+          errMess={this.props.dishes.errMess}
+        />
+        <RenderItem
+          item={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
+          isLoading={this.props.promotions.isLoading}
+          errMess={this.props.promotions.errMess}
+        />
+        <RenderItem
+          item={this.props.leaders.leaders.filter((leader) => leader.featured)[0]}
+          isLoading={this.props.leaders.isLoading}
+          errMess={this.props.leaders.errMess}
+        />
+      </ScrollView>
+    );
+  }
 }
 
 export default connect(mapStateToProps)(Home);
